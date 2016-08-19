f8bec0455b40bf8434f6337c31b11b4f4b764a98
Caroline Joseph <carbroj@gmail.com>
2016-08-19 11:13:25 -0500

    got a toggle but not right yet

--- app/assets/javascripts/dataCtrl.js
+++ app/assets/javascripts/dataCtrl.js
@@ -4,6 +4,9 @@
       $http.get('https://data.cityofchicago.org/resource/uh4d-zh38.json').then(function(response) {
         $scope.bikes = response.data;
       });
+      $scope.changeStatus = function(inputBike) {
+        inputBike.community_name = !inputBike.community_name;
+      };
     };
   });
 })();
